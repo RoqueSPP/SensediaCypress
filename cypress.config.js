14b4af03e0d57fb7268c1ee88ccf30ff03e0b3e8
RoqueSPP <79621434+RoqueSPP@users.noreply.github.com>
2023-06-24 11:04:51 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   projectId: "s48dwu",
-  baseUrl: 'https://api.trello.com/1',
+ 
   e2e: {
-    
+     baseUrl: 'https://api.trello.com/1',
     setupNodeEvents(on, config) {
      
       
